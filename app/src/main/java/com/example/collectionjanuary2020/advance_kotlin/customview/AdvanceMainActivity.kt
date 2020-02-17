0a918ff73d3e8b80bb39cec32f1ecc8490d5c943
nguyenbahung94 <mr.hungcity@gmail.com>
2020-02-17 14:48:35 +0700

    customview

---
+++ app/src/main/java/com/example/collectionjanuary2020/advance_kotlin/customview/AdvanceMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.collectionjanuary2020.advance_kotlin.customview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.collectionjanuary2020.R
+
+class AdvanceMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_customview)
+    }
+}
